57f39fa5a30ecc7e1607077d24b26773f03acaca
brlaney <brlaney@outlook.com>
2021-08-09 10:47:55 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import LineRechartComponent from '../src/components/LineRechartComponent'
+import AreaRechartComponent from '../src/components/AreaRechartComponent';
+import PieRechartComponent from '../src/components/PieRechartComponent';
+import '../src/scss/App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Line Rechart</h2>
+      <LineRechartComponent />
+
+      <h2>Area Rechart</h2>
+      <AreaRechartComponent />
+
+      <h2>Pie Rechart</h2>
+      <PieRechartComponent />
+
+    </div>
+  );
+}
+
+export default App;
