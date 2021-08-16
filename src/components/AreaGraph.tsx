6f59e8c2aea99f2e5e78b373de9d08eca75a19ff
brlaney42 <brlaney@outlook.com>
2021-08-16 12:28:09 -0500

    Added TN Covid data for area graph

--- src/components/AreaGraph.tsx
+++ src/components/AreaGraph.tsx
@@ -23,33 +23,21 @@ class AreaGraph extends React.Component {
       >
         <defs>
           <linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
-            <stop offset='5%' stopColor='#A8293E' stopOpacity={0.8} />
+            <stop offset='5%' stopColor='#A8293E' stopOpacity={0.85} />
             <stop offset='95%' stopColor='#A8293E' stopOpacity={0} />
           </linearGradient>
           <linearGradient id='colorPv' x1='0' y1='0' x2='0' y2='1'>
-            <stop offset='5%' stopColor='#485696' stopOpacity={0.8} />
+            <stop offset='5%' stopColor='#485696' stopOpacity={0.88} />
             <stop offset='95%' stopColor='#485696' stopOpacity={0} />
           </linearGradient>
         </defs>
-        <XAxis dataKey='name' />
+        <XAxis dataKey='date' />
         <YAxis />
         <CartesianGrid strokeDasharray='3 3' />
         <Tooltip />
         <Legend id='legend' />
-        <Area
-          type='monotone'
-          dataKey='Product A'
-          stroke='#A8293E'
-          fillOpacity={1}
-          fill='url(#colorUv)'
-        />
-        <Area
-          type='monotone'
-          dataKey='Procuct B'
-          stroke='#485696'
-          fillOpacity={1}
-          fill='url(#colorPv)'
-        />
+        <Area type='monotone' dataKey='No. cases' stroke='#A8293E' fillOpacity={1} fill='url(#colorUv)' />
+        <Area type='monotone' dataKey='Deaths' stroke='#485696' fillOpacity={1} fill='url(#colorPv)' />
       </AreaChart>
     )
   };
