30b3902b6baab0fe427e30346918d55ce3a8d024
brlaney42 <brlaney@outlook.com>
2021-08-19 08:56:54 -0500

    Adding abortion data types and index

---
+++ src/lib/types/index.ts
@@ -0,0 +1,13 @@
+export interface IInfo {
+  id: number;
+  text: string;
+}
+
+export interface IData {
+  id: number;
+  type: string;
+  title: string;
+  state: string;
+  num: number;
+  percent: number;
+}
